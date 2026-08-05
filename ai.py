9937ec7ce09a25c4bb20ecbff87b8297dab393b7
herbalalamishop08-bit <herbalalamishop08@gmail.com>
2026-08-05 18:08:43 +0700

    Create ai.py

---
+++ ai.py
@@ -0,0 +1,20 @@
+import time
+
+def generate_clip(input_path):
+    """
+    Sementara hanya simulasi proses AI.
+    Nanti akan diganti dengan Whisper + FFmpeg.
+    """
+    time.sleep(3)
+
+    return {
+        "status": "success",
+        "message": "Clip berhasil dibuat"
+    }
+
+
+def generate_subtitle(input_path):
+    """
+    Placeholder untuk Whisper AI.
+    """
+    return "Subtitle akan dibuat di sini."
