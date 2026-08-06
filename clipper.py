6a1923063a8279343b77f438d19f1f8f3acfd3a1
herbalalamishop08-bit <herbalalamishop08@gmail.com>
2026-08-06 18:44:27 +0700

    Add start time slider

--- clipper.py
+++ clipper.py
@@ -1,7 +1,8 @@
 import subprocess
 import os
 
-def make_clip(input_file, output_file, duration=30):
+
+def make_clip(input_file, output_file, duration=30, start_time=0):
 
     os.makedirs(os.path.dirname(output_file), exist_ok=True)
 
@@ -9,6 +10,7 @@ def make_clip(input_file, output_file, duration=30):
         "ffmpeg",
         "-y",
         "-threads", "1",
+        "-ss", str(start_time),
         "-i", input_file,
         "-t", str(duration),
         "-vf", "scale=720:1280:force_original_aspect_ratio=decrease,pad=720:1280:(ow-iw)/2:(oh-ih)/2",
