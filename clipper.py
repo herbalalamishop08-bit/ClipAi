deaa5ae50d351df31217a5c0dd9c993ec9ffe086
herbalalamishop08-bit <herbalalamishop08@gmail.com>
2026-08-06 17:33:41 +0700

    Update clipper.py

--- clipper.py
+++ clipper.py
@@ -1,12 +1,14 @@
 import subprocess
 import os
 
-def make_clip(input_file, output_file):
+def make_clip(input_file, output_file, duration=30):
+    os.makedirs(os.path.dirname(output_file), exist_ok=True)
+
     subprocess.run([
         "ffmpeg",
         "-y",
         "-i", input_file,
-        "-t", "30",
+        "-t", str(duration),
         "-c:v", "libx264",
         "-c:a", "aac",
         output_file
