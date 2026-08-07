50abf56844c6ecd48e03934d053cb96a7b7b4dfe
herbalalamishop08-bit <herbalalamishop08@gmail.com>
2026-08-07 17:53:46 +0700

    Update ai.py

--- ai.py
+++ ai.py
@@ -1,61 +1,39 @@
 import os
-import re
-import google.generativeai as genai
+
 from clipper import make_clip
-
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-
-model = genai.GenerativeModel("gemini-2.5-flash")
+from multi_clip import get_clip_points
 
 OUTPUT_FOLDER = "outputs"
+
 os.makedirs(OUTPUT_FOLDER, exist_ok=True)
-
-
-def get_best_start_time(filename):
-    prompt = f"""
-Video berdurasi beberapa menit.
-
-Tentukan detik terbaik untuk membuat video viral berdurasi pendek.
-
-Jawab HANYA ANGKA.
-Contoh:
-25
-"""
-
-    response = model.generate_content(prompt)
-
-    text = response.text.strip()
-
-    match = re.search(r"\d+", text)
-
-    if match:
-        return int(match.group())
-
-    return 0
 
 
 def generate_clip(input_path, duration=30):
 
     filename = os.path.basename(input_path)
 
-    output_path = os.path.join(
-        OUTPUT_FOLDER,
-        f"clip_{filename}"
-    )
+    clips = []
 
-    start_time = get_best_start_time(filename)
+    for start_time in get_clip_points():
 
-    make_clip(
-        input_path,
-        output_path,
-        duration,
-        start_time
-    )
+        output_path = os.path.join(
+            OUTPUT_FOLDER,
+            f"{start_time}_{filename}"
+        )
+
+        make_clip(
+            input_path,
+            output_path,
+            duration,
+            start_time
+        )
+
+        clips.append(output_path)
 
     return {
         "status": "success",
-        "message": f"✅ Clip dibuat dari detik {start_time}",
-        "output": output_path
+        "message": "✅ 3 Clip berhasil dibuat",
+        "outputs": clips
     }
 
 
